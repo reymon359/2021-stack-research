58e26b0afff277b89c8f8df2f3d9d6a1cc922bea
reymon359 <ramonmorcillocascales@gmail.com>
2021-08-10 18:06:36 +0200

    feat: add choices to update question

--- blitz-tutorial-app/app/questions/mutations/updateQuestion.ts
+++ blitz-tutorial-app/app/questions/mutations/updateQuestion.ts
@@ -5,6 +5,9 @@ import { z } from "zod"
 const UpdateQuestion = z.object({
   id: z.number(),
   text: z.string(),
+  choices: z.array(
+    z.object({id: z.number().optional(), text: z.string()}),
+  )
 })
 
 export default resolver.pipe(
@@ -12,8 +15,25 @@ export default resolver.pipe(
   resolver.authorize(),
   async ({ id, ...data }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const question = await db.question.update({ where: { id }, data })
-
+    const question = await db.question.update({
+      where: {id},
+      data: {
+        ...data,
+        choices: {
+          upsert: data.choices.map((choice) => ({
+            // Appears to be a prisma bug,
+            // because `|| 0` shouldn't be needed
+            where: {id: choice.id || 0},
+            create: {text: choice.text},
+            update: {text: choice.text},
+          })),
+        },
+      },
+      include: {
+        choices: true,
+      },
+    })
+    
     return question
   }
 )
