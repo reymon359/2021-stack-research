addccc7f83d1faf67a05f89127e4ee1265f77597
reymon359 <ramonmorcillocascales@gmail.com>
2021-08-11 07:16:53 +0200

    feat: add layout

--- redwood-tutorial-app/web/src/pages/HomePage/HomePage.js
+++ redwood-tutorial-app/web/src/pages/HomePage/HomePage.js
@@ -1,18 +1,6 @@
-import { Link, routes } from '@redwoodjs/router'
-
 const HomePage = () => {
   return (
     <>
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <main>Home</main>
     </>
   )
